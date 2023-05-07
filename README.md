56037cf50d0d9b092ce9f06c1d8633325f87dcc8
Max Davydenko <maxdavydenkouo@gmail.com>
2023-05-07 20:49:26 +0300

    [add] feature - full autonomous scraping by city page by page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # description
-Service for parsing https://www.daft.ie/property-for-rent/donegal and sending tg notifications about new propositions.
+Service for scraping https://www.daft.ie/property-for-rent/donegal and sending tg notifications about new propositions.
 
 # functionality
 - parse daft site by city
@@ -8,15 +8,17 @@ Service for parsing https://www.daft.ie/property-for-rent/donegal and sending tg
 - notify users in tg via bot about fresh ones
 
 # plan
-[x] basic parsing functionality
-[ ] full autonomous parsing by city
-[ ] store elements
+[x] basic scraping functionality
+[x] full autonomous scraping by city page by page
 [ ] fastapi infrastructure (> service should have runtime)
+[ ] store elements
 [ ] intelligent crontab (try to get only fresh elements)
 [ ] notifications
     [ ] tg bot integration
     [ ] notification view
     [ ] rules for chose which offers should push
+[ ] tests
+[ ] refactor
 [ ] wishes
     [x] fastapi infrastructure
     [ ] sqlalchemy
--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import time
 import json
 import requests
 from bs4 import BeautifulSoup
@@ -13,7 +14,38 @@ ireland_cities = {
 
 
 # ===============================================================================
-def request_daft_property_html(city, page):
+def scraping_loop(city):
+    page = 1
+    while True:
+        # get html
+        html = request_daft_offers_html(city, page)
+
+        # get offers
+        offers = get_offers_from_html(html)
+
+        # errors check
+        if len(offers) == 0 or offers is False:
+            break
+
+        # actuality check
+        # TODO: add
+
+        # write html
+        write_html(build_html_filepath(city, page), html)
+
+        # handle offers
+        handle_offers(offers)
+
+        # stop on the last page
+        if len(offers) < 20:
+            break
+        
+        # increment page
+        page = page + 1
+        time.sleep(5)
+
+
+def request_daft_offers_html(city, page):
     """
     get content from daft.ie site with list of properties for rent
     
@@ -40,29 +72,23 @@ def read_html(filepath):
     with open(filepath, 'r+') as f:
         return f.read()
 
-def parse_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    soup_res = soup.find('script', {"id": "__NEXT_DATA__"}, type='application/json')
-    objects = json.loads(soup_res.contents[0])
-    return objects['props']['pageProps']['listings']
+def get_offers_from_html(html):
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        soup_res = soup.find('script', {"id": "__NEXT_DATA__"}, type='application/json')
+        objects = json.loads(soup_res.contents[0])
+        return objects['props']['pageProps']['listings']
+    except:
+        return False
+    
+def handle_offers(offers):
+    print(len(offers))
 
 
 # ===============================================================================
 def main():
-    page = 1
-    # get html
-    html = request_daft_property_html(ireland_cities[0], page)
-
-    # save html
-    write_html(build_html_filepath(ireland_cities[0], page), html)
+    scraping_loop(ireland_cities[0])
     
-    # read_html
-    html = read_html(build_html_filepath(ireland_cities[0], page))
-
-    # parse html
-    rent_items = parse_html(html)
-    pprint(rent_items[0])
-
 
 # ===============================================================================
 if __name__ == "__main__":
